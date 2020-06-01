ec771f82738e34547826ae301e29158823aedd53
Ashish Chopra <ashish1194.cse16@chitkara.edu.in>
2020-06-01 19:41:06 +0530

    Dynamic ingredients added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import './Burger.css';
+import BurgerIngredient from './Burgeringredient/Burgeringredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i)=>{
+          return <BurgerIngredient key={igKey + i} type={igKey}/>;
+        });
+    });
     return (
-        <div></div>
+        <div className="Burger">
+            <BurgerIngredient type="bread-top" />
+            {/* <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" /> */}
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+            
+        </div>
     );
 };
 export default burger;
